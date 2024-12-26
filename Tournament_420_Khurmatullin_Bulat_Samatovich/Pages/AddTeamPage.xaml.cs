add78f0aa16b0f71f85f2bf12a62a4e2a093b772
Хурматуллин Булат Саматович <202124@mck-ktits.ru>
2024-12-26 15:13:56 +0300

    добавил регистрацию комманд

---
+++ Tournament_420_Khurmatullin_Bulat_Samatovich/Pages/AddTeamPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tournament_420_Khurmatullin_Bulat_Samatovich.DB;
+
+namespace Tournament_420_Khurmatullin_Bulat_Samatovich.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddTeamPage.xaml
+    /// </summary>
+    public partial class AddTeamPage : Page
+    {
+        public AddTeamPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddEmployeeBt_Click(object sender, RoutedEventArgs e)
+        {
+            Team team = new Team();
+            team.Name = NicknameTb.Text;
+            DBConnection.entities.Team.Add(team);
+            DBConnection.entities.SaveChanges();
+            NavigationService.GoBack();
+        }
+    }
+}
